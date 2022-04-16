945a3c0387abf54f21a0afabe725becd344eb667
Jo'raqulov O'tkirjon <97774479+FranchiscoMike@users.noreply.github.com>
2022-04-16 13:43:36 +0500

    Initial commit

---
+++ src/test/java/com/example/lesson07/Lesson07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lesson07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson07ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
